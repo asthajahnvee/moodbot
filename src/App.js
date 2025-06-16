8cfbf39de53129b2a1027296d95c91528aab9d59
asthajahnvee <202101140@daiict.ac.in>
2025-06-16 17:25:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="Home">
+      <h1>MoodBot 😊</h1>
+      <p>Welcome! Tell me how you're feeling today.</p>
+    </div>
+  );
+}
+
+export default App;
